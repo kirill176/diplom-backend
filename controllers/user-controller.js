0fb4bf17c4a335737c977162fd34d3373f42c07c
Siedykh Kyryll <sdsbezoht123@gmail.com>
2025-03-14 21:22:10 +0200

    fix refresh

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -28,6 +28,8 @@ class UserController {
       res.cookie("refreshToken", userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
+        secure: true,
+        sameSite: "none",
       });
       return res.status(200).json(userData);
     } catch (error) {
@@ -38,6 +40,7 @@ class UserController {
   async logout(req, res, next) {
     try {
       const { refreshToken } = req.cookies;
+      console.log(refreshToken);
       const token = await userService.logout(refreshToken);
       res.clearCookie("refreshToken");
       return res.status(200).json(token);
