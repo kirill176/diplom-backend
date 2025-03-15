49733c54583e9f5ebf5c934df3340a13c7293a66
Siedykh Kyryll <sdsbezoht123@gmail.com>
2025-03-15 20:26:36 +0200

    change to getUserData

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,7 +1,8 @@
 const ApiError = require("../exceptions/api-error");
+const userModel = require("../models/user-model");
 const tokenService = require("../service/token-service");
 
-module.exports = function (req, res, next) {
+module.exports = async function (req, res, next) {
   try {
     const authorizationHeader = req.headers.authorization;
     if (!authorizationHeader) {
