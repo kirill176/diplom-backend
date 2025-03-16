3e4370baafd4db0416197e76f5318dce9e711b66
Siedykh Kyryll <sdsbezoht123@gmail.com>
2025-03-16 21:52:16 +0200

    fix

--- models/user-model.js
+++ models/user-model.js
@@ -6,7 +6,7 @@ const User = new Schema({
   userName: { type: String, default: false },
   isActivated: { type: Boolean, default: false },
   activationLink: { type: String },
-  disckSpace: { type: Number, default: 1024 ** 3 * 10 },
+  diskSpace: { type: Number, default: 1024 ** 3 * 10 },
   usedSpace: { type: Number, default: 0 },
   avatar: { type: String },
   files: [{ type: ObjectId, ref: "file" }],
